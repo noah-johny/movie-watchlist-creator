9804e9ca11df826f882f68b5799a1ca340d9ab32
Noah <noahjoh11@gmail.com>
2023-04-14 18:09:29 +0530

    commit after fixing preliminary issues

--- index.js
+++ index.js
@@ -1,82 +1,140 @@
-import watchlistContentWrapper from "./watchlist.js";
-import watchlistBtn from "./watchlist.js";
-
 const searchBtn = document.getElementById("search-btn");
+const homeBtn = document.getElementById("watchlist");
+const watchlistBtn = document.getElementById("home");
 const searchInput = document.getElementById("search-input");
 const contentWrapper = document.getElementById("content-wrapper");
-
+const watchlistContentWrapper = document.getElementById(
+  "watchlist-content-wrapper"
+);
+const myWatchlist = document.getElementById("my-watchlist");
 
 let movieTitle = "";
+let movieCardHTML = "";
+let watchlistMovieCardHTML = "";
+let watchlistArray = [];
+let watchlistPicks = [];
 
-const addedMovies = [];
-let existingMovies = [];
-localStorage.setItem("addedMovies", JSON.stringify(addedMovies));
+let movieCard = document.createElement("div");
+movieCard.classList.add("movie-card");
 
-searchInput.addEventListener("input", (e) => {
-  movieTitle = searchInput.value;
-  renderCards();
-});
+function add(title) {
+  fetch(`https://www.omdbapi.com/?t=${title}&apikey=d09c5b1f`)
+    .then((res) => res.json())
+    .then((data) => {
+      window.localStorage.setItem(data.Title, JSON.stringify(data));
+      watchlistArray.push(data.Title);
+    });
+}
 
-watchlistBtn.addEventListener("click", (e) => {
-  renderWatchlistCards();
-});
+function remove(title) {
+  window.localStorage.removeItem(title);
+  window.location.reload();
+}
 
-function renderWatchlistCards() {
-  watchlistContentWrapper.replaceChildren();
-
-  console.log(localStorage.getItem("addedMovies"));
-
-  watchlistContentWrapper.appendChild(movieCard);
+if (searchInput) {
+  searchInput.addEventListener("input", (e) => {
+    movieTitle = searchInput.value;
+    renderCards();
+  });
 }
 
 function renderCards() {
-  contentWrapper.replaceChildren();
-  console.log(movieTitle);
-  fetch("http://www.omdbapi.com/?apikey=d09c5b1f&s=" + movieTitle)
+  contentWrapper.innerHTML = "";
+  fetch(`http://www.omdbapi.com/?apikey=d09c5b1f&s=${movieTitle}`)
     .then((res) => res.json())
     .then((data) => {
+      if (data.Response === "False") {
+        return;
+      }
       for (let i in data.Search) {
-        console.log(data.Search[i].Title);
         fetch(
-          "http://www.omdbapi.com/?apikey=d09c5b1f&t=" + data.Search[i].Title
+          `http://www.omdbapi.com/?apikey=d09c5b1f&t=${data.Search[i].Title}`
         )
           .then((res) => res.json())
           .then((movie) => {
-            let movieCard = document.createElement("div");
-            movieCard.classList.add("movie-card");
-            movieCard.setAttribute("id", "movie-card-" + i);
-
-            let addWatchlist = "add-watchlist-" + i;
-
-            movieCard.innerHTML = `
-            <img
-                src=${movie.Poster}
-                alt="movie-poster"
-                class="movie-poster"
-              />
-              <div class="details">
-                <div class="title-wrapper">
-                  <h4 class="title">${movie.Title}</h4>
-                  <div class="score">
-                    <i class="fa-solid fa-star"></i>
-\                    <p class="score-value">${movie.imdbRating}</p>
+            movieCardHTML = `
+            <div class="movie-card">
+              <img
+                  src=${movie.Poster}
+                  alt="movie-poster"
+                  class="movie-poster"
+                />
+                <div class="details">
+                  <div class="title-wrapper">
+                    <h4 class="title">${movie.Title}</h4>
+                    <div class="score">
+                      <i class="fa-solid fa-star"></i>
+                      <p class="score-value">${movie.imdbRating}</p>
+                    </div>
                   </div>
+                  <div class="info">
+                    <h5 class="duration">${movie.Runtime}</h5>
+                    <h5 class="genre">${movie.Genre}</h5>
+
+                    <button id="addWatchlist" class="add-watchlist" onclick="add('${movie.Title}')">
+                      + Watchlist
+                    </button>
+
+                  </div>
+                  <p class="description">${movie.Plot}<a>Read More</a>
+                  </p>
                 </div>
-                <div class="info">
-                  <h5 class="duration">${movie.Runtime}</h5>
-                  <h5 class="genre">${movie.Genre}</h5>
-                  <button id=${addWatchlist} class="add-watchlist" onclick="watchlistClick(event)">
-                    +
-                    Watchlist
-                  </button>
-                </div>
-                <p class="description">${movie.Plot}<a>Read More</a>
-                </p>
               </div>
             `;
 
-            contentWrapper.appendChild(movieCard);
+            contentWrapper.innerHTML += movieCardHTML;
           });
       }
     });
+}
+
+function renderWatchlistCards() {
+  let keys = Object.keys(localStorage);
+  let i = keys.length;
+
+  while (i--) {
+    watchlistPicks.push(JSON.parse(localStorage.getItem(keys[i])));
+  }
+
+  if (watchlistPicks.length === 0) {
+    console.log("Nothing to watch");
+  } else {
+    for (let movie of watchlistPicks) {
+      watchlistMovieCardHTML = `
+        <div class="movie-card">
+          <img
+              src=${movie.Poster}
+              alt="movie-poster"
+              class="movie-poster"
+            />
+            <div class="details">
+              <div class="title-wrapper">
+                <h4 class="title">${movie.Title}</h4>
+                <div class="score">
+                  <i class="fa-solid fa-star"></i>
+                  <p class="score-value">${movie.imdbRating}</p>
+                </div>
+              </div>
+              <div class="info">
+                <h5 class="duration">${movie.Runtime}</h5>
+                <h5 class="genre">${movie.Genre}</h5>
+
+                <button id="addWatchlist" class="add-watchlist" onclick="remove('${movie.Title}')">
+                  - Watchlist
+                </button>
+
+              </div>
+              <p class="description">${movie.Plot}<a>Read More</a>
+              </p>
+            </div>
+          </div>
+        `;
+
+      watchlistContentWrapper.innerHTML += watchlistMovieCardHTML;
+    }
+  }
+}
+
+if (watchlistContentWrapper) {
+  renderWatchlistCards();
 }
--- watchlist.js
+++
@@ -1,7 +0,0 @@
-const watchlistBtn = document.getElementsByClassName("add-watchlist");
-
-const watchlistContentWrapper = document.getElementById(
-  "watchlist-content-wrapper"
-);
-console.log(watchlistBtn);
-export default {watchlistContentWrapper, watchlistBtn};
